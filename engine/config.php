cc7beced8ab060cee1f6458b787998c93b8e52d8
maxax14 <50717032+maxax14@users.noreply.github.com>
2019-07-27 21:01:10 +0300

    Update config.php

--- engine/config.php
+++ engine/config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-  'token' => '',
+  'token' => '952902701:AAFBH_GLc7iKSf1Z9ubK5kT6ifdljIwzxvs',
   'api_url' => 'https://api.telegram.org/bot',
   'webhook_url' => 'https://f6f873f0.ngrok.io',
   /*Database*/
